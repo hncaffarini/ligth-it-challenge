c614af1f4aa4070ea01b6c15ffb5bec75b9557a3
H. Nicolás Caffarini <hncaffarini@gmail.com>
2025-03-23 17:07:51 -0300

    feat: Error messages are now animated

--- resources/js/components/input-error.tsx
+++ resources/js/components/input-error.tsx
@@ -1,9 +1,10 @@
+import '../../css/animations.css';
 import { cn } from '@/lib/utils';
 import { type HTMLAttributes } from 'react';
 
 export default function InputError({ message, className = '', ...props }: HTMLAttributes<HTMLParagraphElement> & { message?: string }) {
     return message ? (
-        <p {...props} className={cn('text-sm text-red-600 dark:text-red-400', className)}>
+        <p {...props} className={cn('text-sm text-red-600 dark:text-red-400 animate-slide-in', className)}>
             {message}
         </p>
     ) : null;
